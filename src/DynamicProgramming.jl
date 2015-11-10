a75d2c72c803c41322706f39a58c92b1e587efab
Zac Cranko <zaccranko@gmail.com>
2015-11-11 08:52:08 +1100

    tidy some things

--- src/DynamicProgramming.jl
+++ src/DynamicProgramming.jl
@@ -8,7 +8,8 @@ export UnconstrainedDynamicProgramming
 # bellman things
 export expected_bellman_value, expected_bellman_gradient, optimize_bellman, evaluate_bellman_on_grid, approximate_bellman
 
-include("dynamic_programming.jl")
+include("common.jl")
+include("bellmanoperator.jl")
 include("optimisation.jl")
 
 end # module
