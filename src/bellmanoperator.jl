f78a39fbbc80cf07c193fab786b3daf22fd38c2c
Zac Cranko <zaccranko@gmail.com>
2015-11-15 18:28:10 +1100

    removed some junk

--- src/bellmanoperator.jl
+++ src/bellmanoperator.jl
@@ -94,79 +94,6 @@ function approximate_bellman_operator{T}(d::AbstractDynamicProgramming{T}, shock
     return new_valuefn
 end
 
-#                                iter,state,time,allocd,total_time,gc_dff
-typealias IterationState{T} Tuple{Int, T, Float64, Int, Float64, Base.GC_Diff}
-typealias History{T}        Vector{IterationState{T}}
-
-function pretty_integer(t::Int)
-    @assert t >= 0 "What is this"
-    if    t < 10^2
-        @sprintf "%d" t
-    elseif t < 10^3
-        @sprintf "%.1f×10²" t / 10^2
-    elseif t < 10^4
-        @sprintf "%.1f×10³" t / 10^3
-    elseif t < 10^5
-        @sprintf "%.1f×10⁴" t / 10^4
-    elseif t < 10^6
-        @sprintf "%.1f×10⁵" t / 10^5
-    elseif t < 10^7
-        @sprintf "%.1f×10⁶" t / 10^6
-    elseif t < 10^8
-        @sprintf "%.1f×10⁷" t / 10^7
-    elseif t < 10^9
-        @sprintf "%.1f×10⁸" t / 10^8
-    elseif t < 10^10
-        @sprintf "%.1f×10⁹" t / 10^9
-    else
-        @sprintf "%s"       t
-    end
-end
-
-function verbose(print_level::Int, hist::IterationState)
-    print_level == 0 && return
-
-    i, state, elapsedtime, allocd, gctime, diff = hist
-    iter = pretty_integer_count(i)
-
-    if print_level >= 1
-        n = 100
-        @printf "%s\n" Base.cpad(" Iteration $iter ", n, '=')
-
-        Base.time_print(elapsedtime*1e9, allocd, gctime, Base.gc_alloc_count(diff))
-
-        if print_level >= 2
-            @printf "%s\n" Base.cpad(" Current State ", n, '-')
-            println(state)
-        end
-
-        @printf "%s\n\n" Base.cpad("-", n, '-')
-    end
-end
-
-
-function fixed_iter!{T}(f::Function,
-                        t::Integer,
-                        init::T,
-                        print_level::Int = 0,
-                        trace::Int = 0,
-                        history::History{T} = History{T}())
-    state = init::T
-    for i = 1:t
-        timed = @timed f(state)
-
-        if trace !== 0 && i % trace == 0
-            push!(history, (i, timed...))
-        end
-
-        verbose(print_level, (i, timed...))
-
-        state = first(timed)
-    end
-
-    return state
-end
-
 function iterate_bellman_operator{T}(d::AbstractDynamicProgramming{T}, shocks::Vector, t::Integer; verbose = false)
     init  = approximate_bellman_operator(d, shocks)
 
@@ -175,7 +102,7 @@ function iterate_bellman_operator{T}(d::AbstractDynamicProgramming{T}, shocks::V
         new_state = approximate_bellman_operator(d, old_state, shocks)
 
         if verbose
-            @printf "iteration: %s \tfrobenius norm: %0.4f\t\tapprox. sup norm: %0.4f\n" pretty_integer(i) vecnorm(old_state-new_state) maximum(abs(old_state-new_state))
+            @printf "iteration: %s \tfrobenius norm: %0.2f\tapprox. sup norm: %0.4f\n" pretty_integer(i) vecnorm(old_state-new_state) maximum(abs(old_state-new_state))
         end
 
         old_state = new_state
