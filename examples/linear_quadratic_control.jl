18dcb5e26607406d0495c1e592b2e5ddc40467a9
Zac Cranko <zaccranko@gmail.com>
2015-11-15 18:30:44 +1100

    tidy examples

--- examples/linear_quadratic_control.jl
+++ examples/linear_quadratic_control.jl
@@ -18,13 +18,11 @@ reward(x, i) = -(r*x[1] + x[2] - i[1] - b)^2 - γ*i[1]^2
 end
 initial(state) = zeros(1) # specifies a feasible control for a given state
 
-bounds         = fill(0.0, 1), fill(Inf, 1)
-discretization = (-10.0:10.0, -10.0:10.0)
+grid   = (-10.0:10.0, -10.0:10.0)
 interp = Interpolations.BSpline(Interpolations.Quadratic(Reflect()))
 solver = Ipopt.IpoptSolver(print_level = 0, tol = 1e-2, max_iter=1000)
-lq     = dynamic_programming(reward, transition, initial, β, discretization, 2, 1, interpolation=interp, solver=solver)
+lq     = dynamic_programming(reward, transition, initial, β, grid, 2, 1, interpolation = interp, solver = solver)
 shocks = rand(Normal(0,0.1), 100)
 vfn    = iterate_bellman_operator(lq, shocks, 20, verbose = true)
 
 plot((x,y)->vfn[x,y],-10.0,10,-10.0,10)
-
